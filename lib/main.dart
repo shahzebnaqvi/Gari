584440a52a5b3baff43f9f9828a09ffefb987746
unknown <shahzeb.naqvi12@gmail.com>
2022-01-09 03:16:14 +0500

    testing desgin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gari/screens/home.dart';
+import 'package:gari/screens/sample.dart';
+import 'package:gari/screens/sample2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: Sample2(),
     );
   }
 }
